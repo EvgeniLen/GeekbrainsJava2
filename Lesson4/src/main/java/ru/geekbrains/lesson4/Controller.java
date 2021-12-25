b8f9d790cac45670080d4aa339afcc57f72792ce
EvgeniyLen <evgenilen@mail.ru>
2021-12-25 18:59:36 +0400

    Lesson4 HomeWork1

---
+++ Lesson4/src/main/java/ru/geekbrains/lesson4/Controller.java
@@ -0,0 +1,21 @@
+package ru.geekbrains.lesson4;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    public TextArea mainTextArea;
+    public TextField mainTextField;
+
+
+    public void clickButton(ActionEvent actionEvent) {
+        if (!mainTextField.getText().equals("")){
+            mainTextArea.appendText(mainTextField.getText() + "\n");
+            mainTextField.setText("");
+        }
+
+    }
+}
